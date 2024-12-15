4967e600d9d68adc3242c1383d8d2132cf4b8169
Chris Dexter <8195526+the-dext@users.noreply.github.com>
2024-12-15 16:21:29 +0000

    removed post-sellin reduction quality degrading and took the new sellin value into account as part of initially setting the new quality

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -8,7 +8,6 @@
         private const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
         private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
 
-        private const int SulfurasQuality = 50;
         private const int MaxStandardQuality = 50;
 
         IList<Item> Items;
@@ -21,47 +20,37 @@
         {
             foreach (var item in Items)
             {
-                int maxQualityAllowed = item.Name == Sulfuras 
-                    ? 80 
-                    : MaxStandardQuality;
-
-
-                // degrade quality before degrading sellin
-                item.Quality = item.Name switch
-                {
-                    Sulfuras => item.Quality,
-                    AgedBrie => item.Quality = Math.Min(maxQualityAllowed, item.Quality + 1),
-                    BackstagePasses =>
-                        item.SellIn switch
-                        {
-                            < 6 => Math.Min(maxQualityAllowed, item.Quality + 3),
-                            < 11 => Math.Min(maxQualityAllowed, item.Quality + 2),
-                            _ => Math.Min(maxQualityAllowed, item.Quality + 1),
-                        },
-
-                    _ => item.Quality = Math.Max(0, item.Quality - 1),
-                };
-
-                // reduce sell-in
                 if (item.Name == Sulfuras)
                 {
                     continue;
                 }
-                
-                item.SellIn--;                
 
-                if (item.SellIn >= 0)
-                {
-                    continue;
-                }
-
-                // degrade quality again when passed sell-in date
+                // degrade quality before degrading sellin
                 item.Quality = item.Name switch
                 {
-                    AgedBrie => Math.Min(maxQualityAllowed, item.Quality + 1),
-                    BackstagePasses => 0,
-                    _ => Math.Max(0, item.Quality - 1)
-                };          
+                    Sulfuras => throw new InvalidOperationException("Sulfuras should never change quality"),
+                    
+                    AgedBrie => item.SellIn > 0 
+                        ? Math.Min(MaxStandardQuality, item.Quality + 1)
+                        : Math.Min(MaxStandardQuality, item.Quality + 2),
+
+
+                    BackstagePasses =>
+                        item.SellIn switch
+                        {
+                            <= 0 => 0,
+                            < 6 => Math.Min(MaxStandardQuality, item.Quality + 3),
+                            < 11 => Math.Min(MaxStandardQuality, item.Quality + 2),
+                            _ => Math.Min(MaxStandardQuality, item.Quality + 1),
+                        },
+
+                    _ => item.Quality = item.SellIn > 0 
+                        ? Math.Max(0, item.Quality - 1)
+                        : Math.Max(0, item.Quality - 2)
+                };
+
+                // reduce sell-in                
+                item.SellIn--;   
             }
         }
     }
